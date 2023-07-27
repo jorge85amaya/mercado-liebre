49243865876df269164d0a6e1986042d4cf62c30
Jorge Amaya <jorgebenildoamaya@gmail.com>
2023-07-27 20:33:08 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+
+const app = express();
+const puerto = 3002;
+
+app.use(express.json());
+app.use('/', express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/home.html')
+})
+app.get('/login', (req, res) => {
+    res.sendFile(__dirname + '/views/login.html')
+})
+app.get('/register', (req, res) => {
+    res.sendFile(__dirname + '/views/register.html')
+})
+
+app.listen(puerto, () => {
+    console.log('Aplicación esuchando en puerto 3002');
+});
+
+
